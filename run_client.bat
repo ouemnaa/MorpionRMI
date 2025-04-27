a6b36ab1c204460c26e5a0971096369d983c17a1
ouemnaa <emna.ouertani@etudiant-fst.utm.tn>
2025-04-27 14:09:33 +0100

    Apache works fine all good

--- run_client.bat
+++ run_client.bat
@@ -1,7 +1,8 @@
 @echo off
 REM Configurer le classpath
-set CLASSPATH=out\production\MorpionRMI
+set CLASSPATH=out
 
 REM Lancer le client
-java -cp %CLASSPATH% -Djava.rmi.server.codebase=http://localhost:8000/classes/ -Djava.security.policy=policies\client.policy client.ClientLauncher
+java -cp "out" -D"java.rmi.server.codebase=http://localhost/classes/" -D"java.security.policy=policies/client.policy" client.ClientLauncher
 pause
+
--- run_server.bat
+++ run_server.bat
@@ -3,5 +3,5 @@ REM Configurer le classpath
 set CLASSPATH=out\production\MorpionRMI
 
 REM Lancer le serveur
-java -cp %CLASSPATH% -Djava.rmi.server.codebase=http://localhost:8000/classes/ -Djava.security.policy=policies\server.policy server.GameServer
+java -cp "out" -D"java.rmi.server.codebase=http://localhost/classes/" -D"java.security.policy=policies/server.policy" server.GameServer
 pause
